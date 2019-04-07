5afb721c26fd5d23743795811698d95940c09f0f
Danny Pearson <d.pearson1991@googlemail.com>
2019-04-07 12:27:21 +0100

    Added fluid containers and fixed height/width utility

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const $body = $("body");
 const $footer = $(".sticky-footer");
+const $navbar = $(".navbar");
 const $getChildren = $body.children().not($footer).not("script")
 
 if($footer.length === 1) {
     $getChildren.wrapAll("<div class=\"wrapper\"></div>")
     $(".wrapper").css( { "min-height": $(window).height() - $footer.height() } );;
 }
+
